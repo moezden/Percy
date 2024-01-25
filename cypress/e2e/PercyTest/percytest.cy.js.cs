bd889315826c2a7074cca6bf858f401a725fad98
moezden <moezden@kobo.com>
2024-01-25 13:34:07 +0100

    percyFigma.yml added - figma upload works

--- cypress/e2e/PercyTest/percytest.cy.js.cs
+++ cypress/e2e/PercyTest/percytest.cy.js.cs
@@ -8,9 +8,9 @@ public class percytest_cy_js
     describe('Integration test with visual testing', function() {
         it('Loads the homepage', function() {
             // Load the page or perform any other interactions with the app.
-            cy.visit(www.google.de);
+            cy.visit(www.ikrabau.de);
             // Take a snapshot for visual diffing
-            cy.percySnapshot();
+            cy.percySnapshot('IKRA Homepage', { widths: [768, 992, 1200] });
         });
     });
 }
