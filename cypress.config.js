58b371199eae82f685df17542732239041c24184
moezden <moezden@kobo.com>
2024-01-26 18:15:43 +0100

    ds

---
+++ cypress.config.js
@@ -0,0 +1,30 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    // Your project's URL
+    baseUrl: 'http://localhost:8080',
+
+    // Paths to your integration test files
+    specPattern: 'cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
+
+    // Path to your support files
+    supportFile: 'cypress/support/index.js',
+
+    // Path to your plugins file
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+
+    // Any additional configuration options
+  },
+
+  // Optionally include component testing configuration
+  component: {
+    // Configuration for component tests
+  },
+
+  // Other global configuration options
+  video: false,
+  // ...other configurations
+})
